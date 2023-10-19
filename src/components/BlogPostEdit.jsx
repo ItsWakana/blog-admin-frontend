e4baca24037172840d0397255b4928959ce79a19
itswakana <100365678+ItsWakana@users.noreply.github.com>
2023-10-19 19:00:15 +0100

    add tinycme text widget

--- src/components/BlogPostEdit.jsx
+++ src/components/BlogPostEdit.jsx
@@ -1,8 +1,7 @@
 import { useParams } from "react-router-dom";
 import useBlogPageInfo from "../hooks/useBlogPageInfo";
-import { useEffect, useState } from "react";
-
-import CommentSection from "./CommentSection";
+import { useEffect, useState, useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
 const BlogPostEdit = ({ validatePostEdit }) => {
 
@@ -14,6 +13,9 @@ const BlogPostEdit = ({ validatePostEdit }) => {
     const [blogContent, setBlogContent] = useState('');
     const [publishState, setPublishState] = useState('');
     const [inputError, setInputError] = useState({for: '', error: ''});
+
+    const editorRef = useRef(null);
+
     useEffect(() => {
 
         if (blogItem && blogItem.title && blogItem.content) {
@@ -22,6 +24,12 @@ const BlogPostEdit = ({ validatePostEdit }) => {
             setPublishState(blogItem.published.isPublished ? 'published' : 'unpublished');
         }
     },[blogItem])
+
+    const log = () => {
+      if (editorRef.current) {
+        console.log(editorRef.current.getContent());
+      }
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -57,8 +65,30 @@ const BlogPostEdit = ({ validatePostEdit }) => {
                     </div>
                     <div>
                         <label htmlFor="content">Content</label>
-                        <textarea name="content" cols="30" rows="10" value={blogContent} onChange={(e) => setBlogContent(e.target.value)}
-                        className={inputError.for === 'content' ? 'input-error' : ''}></textarea>
+                        {/* <textarea name="content" cols="30" rows="10" value={blogContent} onChange={(e) => setBlogContent(e.target.value)}
+                        className={inputError.for === 'content' ? 'input-error' : ''}></textarea> */}
+                        <Editor
+                            apiKey='9co21oru5yksr9rwycxaaw02543pyof3bnomvhipsg5ghl91'
+                            onInit={(evt, editor) => editorRef.current = editor}
+                            value={`<p>${blogContent}</p>`}
+                            onEditorChange={(newValue, editor) => {
+                                setBlogContent(editor.getContent({ format: 'text'}));
+                            }}
+                            init={{
+                            height: 500,
+                            menubar: false,
+                            plugins: [
+                                'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                                'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+                                'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+                            ],
+                            toolbar: 'undo redo | blocks | ' +
+                                'bold italic forecolor | alignleft aligncenter ' +
+                                'alignright alignjustify | bullist numlist outdent indent | ' +
+                                'removeformat | help',
+                            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                            }}
+                        />
                     </div>
                     <div>
                     <label htmlFor="published">Published</label>
